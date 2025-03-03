2b8cee1033105df0e80dc7e72b4d97caf562ba21
dpichamba <dorivalpichamba1996@gmail.com>
2025-03-03 00:48:38 -0500

    metodo para capturar la posicion de manos puntos x,y

--- mouse_control.py
+++ mouse_control.py
@@ -2,9 +2,24 @@ import cv2
 import mediapipe
 import pyautogui
 
+captura_manos=mediapipe.solutions.hands.Hands() #capturo las manos
+op_dibujo=mediapipe.solutions.drawing_utils #dibujo los movimientos
+
+
 cam=cv2.VideoCapture(0) #utilizo la primera camara de mi pc
 while True:
     _,image = cam.read() #capturo la camara
+    image=cv2.flip(image,1) #posicion 1 eje vert
+    rgb_image= cv2.cvtColor(image,cv2.COLOR_BGR2RGB) #convertir imagen a rgb
+    salida_manos= captura_manos.process(rgb_image)#capturo las manos
+    all_hands= salida_manos.multi_hand_landmarks #capturo todas las manos
+
+    #valida si hay manos
+    if all_hands:
+        #que capture una por una
+        for hand in all_hands:
+            op_dibujo.draw_landmarks(image,hand)
+
     cv2.imshow("captura de video de los movimientos de la mano",image)
     key=cv2.waitKey(100)
     if key == 27:
